1f02af39956ad9246df1bb8b12eefa09afc06687
JoaoPDeveloper <joao_entreprise@hotmail.com>
2023-06-09 20:54:49 -0300

    mudanças no layout geral

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,61 +1,71 @@
-var i = setInterval(function () { 
+var i = setInterval(function () {
   clearInterval(i);
   document.getElementById("carregando").style.display = "none";
-      document.getElementById("container").style.display = "inline";
-    }, 1000);
-    ScrollReveal().reveal('.headline',{delay:300})
-		ScrollReveal().reveal('.container-about', { delay: 800 })
-		ScrollReveal().reveal('.container', { delay: 800 })
-    ScrollReveal().reveal('.square', { delay: 800 })  
-    window.onscroll = function() {
-      var backToTopBtn = document.querySelector('.back-to-top');
-      if (window.pageYOffset > 300) {
-        backToTopBtn.style.display = 'block';
-      } else {
-        backToTopBtn.style.display = 'none';
-      }
-    };
-    function backToTop() {
-      window.scrollTo(0, 0);
-    }
-    document.getElementById('botaoMostrar').addEventListener('click', function() {
-  document.querySelector('.conteudo').classList.add('mostrando');
+  document.getElementById("container").style.display = "inline";
+}, 1000);
+ScrollReveal().reveal(".headline", { delay: 300 });
+ScrollReveal().reveal(".container-about", { delay: 800 });
+ScrollReveal().reveal(".container", { delay: 800 });
+ScrollReveal().reveal(".square", { delay: 800 });
+window.onscroll = function () {
+  var backToTopBtn = document.querySelector(".back-to-top");
+  if (window.pageYOffset > 300) {
+    backToTopBtn.style.display = "block";
+  } else {
+    backToTopBtn.style.display = "none";
+  }
+};
+function backToTop() {
+  window.scrollTo(0, 0);
+}
+document.getElementById("botaoMostrar").addEventListener("click", function () {
+  document.querySelector(".conteudo").classList.add("mostrando");
 });
 
-document.getElementById('botaoMostrar').addEventListener('click', function(event) {
-  event.preventDefault();
-  var elementoDestino = document.getElementById('container-about');
-  var offsetTop = elementoDestino.offsetTop;
-  var deslocamento = offsetTop - 100; 
-  window.scrollTo({
-    top: deslocamento,
-    behavior: 'smooth' 
+document
+  .getElementById("botaoMostrar")
+  .addEventListener("click", function (event) {
+    event.preventDefault();
+    var elementoDestino = document.getElementById("container-about");
+    var offsetTop = elementoDestino.offsetTop;
+    var deslocamento = offsetTop - 100;
+    window.scrollTo({
+      top: deslocamento,
+      behavior: "smooth",
+    });
   });
-});
 
 const bodyEl = document.body;
-const barEl = document.querySelector('.bar');
+const barEl = document.querySelector(".bar");
 const updateBar = () => {
-  let scrollPos = (window.scrollY / (bodyEl.scrollHeight - window.innerHeight)) * 100;
+  let scrollPos =
+    (window.scrollY / (bodyEl.scrollHeight - window.innerHeight)) * 100;
   barEl.style.width = `${scrollPos}%`;
   requestAnimationFrame(updateBar);
 };
 
 updateBar();
-const javascriptProgress = document.querySelector('.javascript progress');
-const typescriptProgress = document.querySelector('.typescript progress');
-const phpProgress = document.querySelector('.progress-bar.progress-bar progress');
-const angularProgress = document.querySelector('.progress-bar:nth-of-type(2) progress');
-const sqlProgress = document.querySelector('.progress-bar:last-of-type progress');
-const tailwindProgress = document.querySelector('.progress-bar:last-of-type progress');
+const javascriptProgress = document.querySelector(".javascript progress");
+const typescriptProgress = document.querySelector(".typescript progress");
+const phpProgress = document.querySelector(
+  ".progress-bar.progress-bar progress"
+);
+const angularProgress = document.querySelector(
+  ".progress-bar:nth-of-type(2) progress"
+);
+const sqlProgress = document.querySelector(
+  ".progress-bar:last-of-type progress"
+);
+const tailwindProgress = document.querySelector(
+  ".progress-bar:last-of-type progress"
+);
 
-javascriptProgress.style.setProperty('--progress-value', '65');
-typescriptProgress.style.setProperty('--progress-value', '45');
-phpProgress.style.setProperty('--progress-value', '35');
-angularProgress.style.setProperty('--progress-value', '35');
-sqlProgress.style.setProperty('--progress-value', '75');
-tailwindProgress.style.setProperty('--progress-value', '35');
-
+javascriptProgress.style.setProperty("--progress-value", "65");
+typescriptProgress.style.setProperty("--progress-value", "45");
+phpProgress.style.setProperty("--progress-value", "35");
+angularProgress.style.setProperty("--progress-value", "35");
+sqlProgress.style.setProperty("--progress-value", "75");
+tailwindProgress.style.setProperty("--progress-value", "35");
 
 function openPokedexModal() {
   document.getElementById("modal").style.display = "block";
@@ -99,5 +109,3 @@ function openLoadinModal() {
 function closeLoadingModal() {
   document.getElementById("modal-carregar").style.display = "none";
 }
-
-
