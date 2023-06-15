35dcf8d47f85a26a93ff781f8bdcd546797ed914
JoaoPDeveloper <joao_entreprise@hotmail.com>
2023-06-15 00:20:19 -0300

    mudanças no layout por completo e adição de novas funçções

--- assets/js/listaProjetos.js
+++ assets/js/listaProjetos.js
@@ -3,9 +3,12 @@ let listaProjetos = [
     nome: "Pokedex",
     img: "https://i.ibb.co/g7wkCx7/pokedex.png",
     descricao:
-      "Esse projeto foi um grande desafio puxar os dados da Api e aplicar no front-end, porém com muito estudo e dedicação, consegui concluir ele de maneira até que rápida, Adicionei um Modal para cada Pokemon e utilizando a cor de cada elemento que também é puxada da API!Tecnologias utilizadas: JavaScript, HTML, CSS3 e APi do Pokemon!",
+      "Esse projeto foi um grande desafio puxar os dados da Api e aplicar no front-end, porém com muito estudo e dedicação, consegui concluir ele de maneira até que rápida, Adicionei um Modal para cada Pokemon e utilizando a cor de cada elemento que também é puxada da API!Tecnologias utilizadas:       JavaScript, HTML, CSS3 e APi do Pokemon!",
     linkProjeto: "https://pokedex-joaopdeveloper.netlify.app/",
     linkGithub: "https://github.com/JoaoPDeveloper/pokedex-lab",
+    tecnologia1: 'https://upload.wikimedia.org/wikipedia/commons/6/6a/JavaScript-logo.png',
+    tecnologia2: 'https://www.vectorlogo.zone/logos/w3_css/w3_css-icon.svg',
+    tecnologia3: 'https://www.vectorlogo.zone/logos/w3_html5/w3_html5-icon.svg',
   },
   {
     nome: "BuzzFeed",
@@ -14,6 +17,9 @@ let listaProjetos = [
       "Projeto de um Clone do Buzz-feed utilizando TypeScript, Angular e Json para gerar novas perguntas eno final dar um resultado para o usuário, o Grande desafio desse projeto foi fazer as rotas para as perguntas e puxar um resultado final para o usuário,Site ficou totalmente responsivo sendo perfeitopara se utilizar em Mobile, utilizei o Vercel para dar o deploy e host para o site! Tecnologias utilizadas: JavaScript,TypeScript,Angular, HTML5 e CSS3",
     linkProjeto: "https://joaopdeveloper-buzzquizz.vercel.app/",
     linkGithub: "https://github.com/JoaoPDeveloper/buzz-quiz-angular",
+    tecnologia1: 'https://www.vectorlogo.zone/logos/typescriptlang/typescriptlang-icon.svg',
+    tecnologia2: 'https://www.vectorlogo.zone/logos/w3_css/w3_css-icon.svg',
+    tecnologia3: 'https://upload.wikimedia.org/wikipedia/commons/c/cf/Angular_full_color_logo.svg',
   },
   {
     nome: "Mundo Invertido",
@@ -22,6 +28,9 @@ let listaProjetos = [
       "Projeto Feito em comemoração ao lançamento da Quarta Temporada de Stranger Things, o mundo se inverte de acordo com o que o usuário decide a partir do botão e no final da pagina, tem um formulario para ousuário colocar a descrição do seu personagem! Tecnologias utilizadas: JavaScript, HTML, CSS3 eGoogle FireBase",
     linkProjeto: "https://inverted-world-z4rk01.netlify.app/",
     linkGithub: "https://github.com/JoaoPDeveloper/mundo-invertido",
+    tecnologia1: 'https://upload.wikimedia.org/wikipedia/commons/6/6a/JavaScript-logo.png',
+    tecnologia2: 'https://www.vectorlogo.zone/logos/w3_css/w3_css-icon.svg',
+    tecnologia3: 'https://www.vectorlogo.zone/logos/w3_html5/w3_html5-icon.svg',
   },
   {
     nome: "Game Mario",
@@ -30,6 +39,9 @@ let listaProjetos = [
       "Game do Mário com a musica original e botão de restart para o usuário reiniciar a sessão, Tecnologias utilizadas: JavaScript, HTML, CSS3!",
     linkProjeto: "https://joaopdeveloper-mario-game.vercel.app/",
     linkGithub: "https://github.com/JoaoPDeveloper/javascript-mario-game",
+    tecnologia1: 'https://upload.wikimedia.org/wikipedia/commons/6/6a/JavaScript-logo.png',
+    tecnologia2: 'https://www.vectorlogo.zone/logos/w3_css/w3_css-icon.svg',
+    tecnologia3: 'https://www.vectorlogo.zone/logos/w3_html5/w3_html5-icon.svg',
   },
   {
     nome: "Conversor Dólar ",
@@ -38,6 +50,9 @@ let listaProjetos = [
       "Conversor de Dolares para Real utilizando a API do Banco Central que fornece os dados da moeda em tempo real, Projeto Feito totalmente em PHP , Tecnologias utilizadas: PHP, HTML, CSS3 e APi do banco central!",
     linkProjeto: "#",
     linkGithub: "",
+    tecnologia1: 'https://www.php.net/images/logos/new-php-logo.svg',
+    tecnologia2: 'https://www.vectorlogo.zone/logos/w3_css/w3_css-icon.svg',
+    tecnologia3: 'https://www.vectorlogo.zone/logos/w3_html5/w3_html5-icon.svg',
   },
   {
     nome: "Simulador Financeiro",
@@ -46,6 +61,9 @@ let listaProjetos = [
       "Projeto que trás informações e calculo baseado no valor que o usuario fornece, juntamente com o valorde entrada, porcentagem de juros e Prazo para a finalização do processo, Foi Adicionado uma novafunção que é a Carência, com ela o usuário decide se quer a carência no eu financiamento e aquantidade de meses para o inicio da mesma!Tecnologias utilizadas: JavaScript, HTML, CSS3",
     linkProjeto: "https://simulador-financeamento.vercel.app/",
     linkGithub: "https://github.com/JoaoPDeveloper/simulador-financeamento",
+    tecnologia1: 'https://upload.wikimedia.org/wikipedia/commons/6/6a/JavaScript-logo.png',
+    tecnologia2: 'https://www.vectorlogo.zone/logos/w3_css/w3_css-icon.svg',
+    tecnologia3: 'https://www.vectorlogo.zone/logos/w3_html5/w3_html5-icon.svg',
   },
   // {
   //  nome: "",
@@ -60,3 +78,11 @@ let listaProjetos = [
   //  linkProjeto: ""
   // },
 ];
+const tecnologias =  {
+  javascript:'https://upload.wikimedia.org/wikipedia/commons/6/6a/JavaScript-logo.png',
+  typescript: 'https://www.vectorlogo.zone/logos/typescriptlang/typescriptlang-icon.svg',
+  css: 'https://www.vectorlogo.zone/logos/w3_css/w3_css-icon.svg',
+  html: 'https://www.vectorlogo.zone/logos/w3_html5/w3_html5-icon.svg',
+  angular: 'https://upload.wikimedia.org/wikipedia/commons/c/cf/Angular_full_color_logo.svg',
+  php: 'https://www.php.net/images/logos/new-php-logo.svg'
+}
--- assets/js/projetos.js
+++ assets/js/projetos.js
@@ -3,10 +3,10 @@ listaProjetos.map((projeto, posicao) => {
 let card = `
     <div class="col-md-4">
         <div class="card m-2" onclick="zoomImg('${posicao}')" title="Clique aqui para obter detalhes!" style="cursor: pointer;">
-            <img src="${projeto.img}" id="imgProjeto" class="card-img-top" alt="Imagem do projeto">
-            <div class="card-body">
-                <h5 class="card-title">${projeto.nome}</h5>
-                <button class="btn btn-light btn-sm" onclick="zoomImg('${posicao}')" id="botaoCorProjeto" title="Clique aqui para verificar o Projeto">Sobre o Projeto.</button>
+        <img src="${projeto.img}" id="imgProjeto" class="card-img-top" alt="Imagem do projeto">
+        <div class="card-body">
+        <h5 class="card-title" id="nomeProjeto" style="color:red">${projeto.nome}</h5>
+        <button class="btn btn-light btn-sm" onclick="zoomImg('${posicao}')" id="botaoCorProjeto" title="Clique aqui para verificar o Projeto">Sobre o Projeto.</button>
                 </div>
                 </div>
                 </div>
@@ -19,6 +19,9 @@ let projeto = listaProjetos[posicao];
 document.querySelector("#modalZoomImgNomeProjeto").innerHTML = projeto.nome;
 document.querySelector("#modalZoomImgDescricaoProjeto").innerHTML = projeto.descricao;
 document.querySelector("#modalZoomImgProjeto").src = projeto.img;
+document.querySelector("#tecnologia1").src  = projeto.tecnologia1;
+document.querySelector("#tecnologia2").src  = projeto.tecnologia2;
+document.querySelector("#tecnologia3").src  = projeto.tecnologia3;
 document.querySelector('#ButtonProjeto').href = projeto.linkProjeto;
 document.querySelector('#ButtonGitHub').href = projeto.linkGithub;
 new bootstrap.Modal('#modalZoomImg').show();
@@ -29,21 +32,25 @@ function alterarTemaSite() {
  
  if (tema === 'dark') {
    document.querySelector("html").setAttribute("data-bs-theme", 'light');
-   document.querySelector("#alterarTemaSite").innerHTML = `Dark Mode <i class="bi bi-moon-stars"></i>`;
+   document.querySelector("#alterarTemaSite").innerHTML = ` <i class="bi bi-moon-stars"></i>`;
    document.querySelector("#textAbout").style.color = "white"
    document.querySelector("#textoAbout").style.color = 'white'
    document.querySelector("#aboutText").style.color = 'white'
    document.querySelector("#textoInicial").style.color = "black"
    document.querySelector("#textoPrincipal").style.color = "black"
-
+   document.querySelector(".icon3 img").src = "/assets/img/icon3.svg";
+   document.querySelector("#nomeProjeto").style.color = 'white';
+   
  } else {
    document.querySelector("html").setAttribute("data-bs-theme", 'dark');
-   document.querySelector("#alterarTemaSite").innerHTML = `Light Mode <i class="bi bi-brightness-high-fill">`;
+   document.querySelector("#alterarTemaSite").innerHTML = `<i class="bi bi-brightness-high-fill"></i>`;
    document.querySelector("#textAbout").style.color = "yellow"
    document.querySelector("#textoAbout").style.color = 'yellow'
    document.querySelector("#aboutText").style.color = 'yellow'
    document.querySelector("#textoInicial").style.color = "yellow"
    document.querySelector("#textoPrincipal").style.color = "white"
+   document.querySelector(".icon3 img").src = "/assets/img/icon3.svg";
+   document.querySelector("#nomeProjeto").style.color = 'white';
  }
  
  let botoes = document.querySelectorAll("#botaoCorProjeto");
